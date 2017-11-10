abc5c94829aedca6e6fe02110eb35a23b3194138
sagar masuti <sagar.masuti@gmail.com>
2017-11-10 11:12:34 +0800

    made getmodel.sh a bit general

--- myscripts/getmodel.sh
+++ myscripts/getmodel.sh
@@ -1,16 +1,48 @@
 #!/usr/bin/env bash
 
-#scp sagarshr001@komodo.ase.ntu.edu.sg:/mnt/isilon/userdata/home/sagarshr001/workspace/wharton12/miracle/$1/stdout .
-scp sagarshr@ntu.nscc.sg:/home/users/ntu/sagarshr/workspace/wharton12/miracle/$1/stdout .
-#relax="relax_"`echo $2 | awk '{printf "%04.4d",$1}'`
-#echo $relax
-#scp mechanics@komodo.ase.ntu.edu.sg:~//wharton12/$1/stdout .
-#scp mechanics@komodo.ase.ntu.edu.sg:~/wharton12/$1/$relax/*.ned .
-mv stdout $1.dat
+eval "$(docopts -V - -h - : "$@" <<EOF
+Usage: getmodel.sh [options]
+
+      -s, --source=<SOURCE>       The server name.
+      -m, --model=<MODEL>         The name of the folder/model.
+      --help                      Show help options.
+      --version                   Print program version.
+
+Description:
+     This script downloads the stdout of the model and changes to the format 
+     required for plotting.
+
+Examples:
+
+----
+getmodel.sh 1.0
+opyright (C) 2017 Sagar Masuti
+Author: Sagar Masuti 
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+EOF
+)"
+
+case $source in 
+[1])
+scp sagarshr001@komodo.ase.ntu.edu.sg:/mnt/isilon/userdata/home/sagarshr001/workspace/wharton12/miracle/$model/stdout .
+;;
+[2])
+scp sagarshr@ntu.nscc.sg:/home/users/ntu/sagarshr/workspace/wharton12/miracle/benchmark_3_param/$model/stdout .
+;;
+[3])
+scp sagarshr001@nya2.hpc.ntu.edu.sg:/home/sagarshr001/workspace/wharton12/miracle/$model/stdout .
+;;
+*)
+echo "No matching cluster found"
+;;
+esac
+
+mv stdout $model.dat
 # if 3 model parameter add $6 to the below awk.
-expnum=`echo $1 | cut -d"_" -f 3`
-size=`echo $1 | cut -d"_" -f 2`
-cat $1.dat | grep "in.param" | awk 'BEGIN{print "\%n    misfit    m1(coh)    m2(V_0)"}{print NR,$2,$4,$5,$6}' > temp_$1.dat
-newfilename=`echo $1.dat | sed s/miracle/wharton/g`
-mv temp_$1.dat $newfilename
+expnum=`echo $model | cut -d"_" -f 3`
+size=`echo $model | cut -d"_" -f 2`
+cat $model.dat | grep "in.param" | awk 'BEGIN{print "\%n    misfit    m1(coh)    m2(V_0)"}{print NR,$2,$4,$5,$6}' > temp_$model.dat
+newfilename=`echo $model.dat | sed s/miracle/wharton/g`
+mv temp_$model.dat $newfilename
 echo "created $newfilename file"
